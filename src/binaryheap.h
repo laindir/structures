716ef557e956a4035939e37cff3fee544952fb1b
Carl D Hamann <carl.hamann@gmail.com>
2012-12-11 12:54:19 -0600

    Added license information

--- src/binaryheap.h
+++ src/binaryheap.h
@@ -1,3 +1,24 @@
+/*
+
+Copyright 2012 Carl D Hamann
+
+This file is part of structures.
+
+structures is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+structures is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with structures.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 #ifndef BINARYHEAP_H
 #define BINARYHEAP_H
 
